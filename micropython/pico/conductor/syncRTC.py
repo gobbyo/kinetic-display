7502bdd10897f4431906fe74e725983492cd253e
gobbyo <jbeman@hotmail.com>
2025-05-05 17:36:17 -0700

    update to rtc

--- micropython/pico/conductor/syncRTC.py
+++ micropython/pico/conductor/syncRTC.py
@@ -2,11 +2,9 @@ from machine import RTC
 import urequests
 import ujson
 
-externalIPAddressAPI = const("http://api.ipify.org")
-externalWorldTimeAPI = const("http://worldtimeapi.org/api/ip")
-externalOpenTimeAPI = const("https://www.timeapi.io/api/Time/current/ip?ipAddress={0}")
+externalIPAddressAPI=const("http://api.ipify.org")
 
-# This class is used to sync the RTC with the WorldTimeAPI service
+# This class is used to sync the RTC with the timeapi.io service
 # It is also used to obtain the external IP address of the device
 # in order to determine the timezone of the device
 class syncRTC:
@@ -16,68 +14,40 @@ class syncRTC:
 
     def syncclock(self, rtc):
         print("Sync clock")
-        self.setExternalIPAddress()
-
-        # List of APIs to try in order
-        apis = [
-            externalWorldTimeAPI,
-            externalOpenTimeAPI.format(self.externalIPaddress)
-        ]
-
-        for api in apis:
-            try:
-                r = urequests.get(api)
-                z = ujson.loads(r.content)
-                print(f"API Response from {api}: {z}")
-
-                # Handle response from TimeAPI.io
-                if api.startswith("https://www.timeapi.io"):
-                    dt = (
-                        int(z["year"]),
-                        int(z["month"]),
-                        int(z["day"]),
-                        0,  # Weekday (set to 0, can be calculated if needed)
-                        int(z["hour"]),
-                        int(z["minute"]),
-                        int(z["seconds"]),  # Note: "seconds" key in TimeAPI.io response
-                        0  # Subseconds set to 0
-                    )
-                else:
-                    # Handle response from WorldTimeAPI
-                    iso_time = z["datetime"]  # Example: "2025-04-28T20:44:06.487506+00:00"
-                    dt = self._iso_to_rtc_tuple(iso_time)
-
-                # Set the RTC datetime
-                rtc.datetime(dt)
-                return True
-            except Exception as e:
-                print(f"Exception with API {api}: {e}")
-
-        # If all APIs fail
-        return False
-
-    def _iso_to_rtc_tuple(self, iso_time):
-        """Convert ISO 8601 string to RTC-compatible tuple."""
-        date_part, time_part = iso_time.split("T")
-        year, month, day = map(int, date_part.split("-"))
-        time_parts = time_part.split(":")
-        hour = int(time_parts[0])
-        minute = int(time_parts[1])
-        second = int(time_parts[2].split(".")[0])  # ignore milliseconds
-
-        # Weekday is set to 0 (can be calculated if needed)
-        return (year, month, day, 0, hour, minute, second, 0)
-
-    def setExternalIPAddress(self):
         returnval = True
+
         try:
-            ipaddress = urequests.get(externalIPAddressAPI)
-            self.externalIPaddress = ipaddress.content.decode("utf-8")
+            self.setExternalIPAddress()
+            timeAPI = "https://www.timeapi.io/api/Time/current/ip?ipAddress={0}".format(self.externalIPaddress)
+            r = urequests.get(timeAPI)
+            z = ujson.loads(r.content)
+            timeAPI = "https://www.timeapi.io/api/TimeZone/zone?timeZone={0}".format(z["timeZone"])
+            print(timeAPI)
+            rq = urequests.get(timeAPI)
+            j = ujson.loads(rq.content)
+            t = (j["currentLocalTime"].split('T'))[1].split(':')
+            print(t)
+            #[year, month, day, weekday, hours, minutes, seconds, subseconds]
+            rtc.datetime((int(z["year"]), int(z["month"]), int(z["day"]), 0, int(t[0]), int(t[1]), int(z["seconds"]), 0))
         except Exception as e:
             print("Exception: {}".format(e))
             returnval = False
         finally:
-            returnval
+            return returnval
+
+    def __del__(self):
+        pass
+
+    def setExternalIPAddress(self):
+        try:
+            print("Obtaining external IP Address")
+            ipaddress = urequests.get(externalIPAddressAPI)
+            self.externalIPaddress = ipaddress.content.decode("utf-8")
+            print("Obtained external IP Address: {0}".format(self.externalIPaddress))
+        except Exception as e:
+            print("Exception: {}".format(e))
+        finally:
+            pass
 
 #Example usage:
 def main():
--- micropython/pico/conductor/testAPIs.py
+++ micropython/pico/conductor/testAPIs.py
@@ -23,27 +23,79 @@ try:
         sync.setExternalIPAddress()
         ext_ip = sync.externalIPaddress
         if ext_ip != prev_ip:
-            print(f"Test 1: Success. Obtained new external IP address from {syncRTC.externalIPAddressAPI}")
+            print(f"Success. Obtained new external IP address from {syncRTC.externalIPAddressAPI}")
             print(f"External IP address: {ext_ip}")
             print("**************************")
         else:
-            print(f"Test 1: Failed. No external IP address obtained from {syncRTC.externalIPAddressAPI}")
+            print(f"Failed. No external IP address obtained from {syncRTC.externalIPAddressAPI}")
             print("**************************")
         time.sleep(1)
-
+        
         print("\n*******API Test 2*********")
         prev_dt = rtc.datetime()
+        
+        # Test 2: Test syncclock() with main API endpoint
+        rtc.datetime((1970, 1, 1, 0, 0, 0, 0, 0)) # Set RTC to epoch time
         sync.syncclock(rtc)
         dt = rtc.datetime()
-        if dt[0] != prev_dt[0]:
-            print(f"Test 2: Success. RTC synced with external time API {syncRTC.externalWorldTimeAPI} or {syncRTC.externalOpenTimeAPI}")
+        if dt[0] != 1970:
+            print(f"Success. RTC synced with external time API {syncRTC.externalWorldTimeAPI} or {syncRTC.externalOpenTimeAPI}")
             print(f"DateTime. \n\tyear: {dt[0]}\n\tmonth: {dt[1]}\n\tday: {dt[2]}\n\tweekday: {dt[3]}\n\thours: {dt[4]}\n\tminutes: {dt[5]}\n\tseconds: {dt[6]}")
             print("**************************")         
         else:
-            print(f"Test 2: Failed. Could not obtain external time from {syncRTC.externalWorldTimeAPI} or {syncRTC.externalOpenTimeAPI}")
+            print(f"Failed. Could not obtain external time from {syncRTC.externalWorldTimeAPI} or {syncRTC.externalOpenTimeAPI}")
             print("**************************")
         
+        # Test 3: Test with specific timezone name - America/Los_Angeles
         print("\n*******API Test 3*********")
+        rtc.datetime((1970, 1, 1, 0, 0, 0, 0, 0)) # Reset RTC to epoch time
+        timezone_to_test = "America/Los_Angeles"
+        if sync.syncclockWithTimeZone(rtc, timezone_to_test):
+            dt = rtc.datetime()
+            if dt[0] != 1970:
+                print(f"Success. RTC synced with TimeAPI.io using timezone {timezone_to_test}")
+                print(f"DateTime ({timezone_to_test}). \n\tyear: {dt[0]}\n\tmonth: {dt[1]}\n\tday: {dt[2]}\n\tweekday: {dt[3]}\n\thours: {dt[4]}\n\tminutes: {dt[5]}\n\tseconds: {dt[6]}")
+                print("**************************")
+            else:
+                print(f"Failed. Could not sync time using TimeAPI.io with timezone {timezone_to_test}")
+                print("**************************")
+        else:
+            print(f"Failed. Could not connect to TimeAPI.io with timezone {timezone_to_test}")
+            print("**************************")
+        
+        # Test 4: Test with another timezone - Europe/London
+        print("\n*******API Test 4*********")
+        rtc.datetime((1970, 1, 1, 0, 0, 0, 0, 0)) # Reset RTC to epoch time
+        timezone_to_test = "Europe/London"
+        if sync.syncclockWithTimeZone(rtc, timezone_to_test):
+            dt = rtc.datetime()
+            if dt[0] != 1970:
+                print(f"Success. RTC synced with TimeAPI.io using timezone {timezone_to_test}")
+                print(f"DateTime ({timezone_to_test}). \n\tyear: {dt[0]}\n\tmonth: {dt[1]}\n\tday: {dt[2]}\n\tweekday: {dt[3]}\n\thours: {dt[4]}\n\tminutes: {dt[5]}\n\tseconds: {dt[6]}")
+                print("**************************")
+            else:
+                print(f"Failed. Could not sync time using TimeAPI.io with timezone {timezone_to_test}")
+                print("**************************")
+        else:
+            print(f"Failed. Could not connect to TimeAPI.io with timezone {timezone_to_test}")
+            print("**************************")
+            
+        # Test 5: Test saving timezone setting to config
+        print("\n*******API Test 5*********")
+        test_timezone = "Asia/Tokyo"
+        conf = Config("config.json")
+        conf.write("timeZone", test_timezone)
+        new_sync = syncRTC.syncRTC("config.json")
+        
+        if new_sync.timeZone == test_timezone:
+            print(f"Success. Timezone {test_timezone} was saved to config and loaded successfully")
+            print("**************************")
+        else:
+            print(f"Failed. Timezone {test_timezone} was not properly stored or retrieved")
+            print(f"Retrieved value: {new_sync.timeZone}")
+            print("**************************")
+        
+        print("\n*******API Test 6*********")
         tempHumid = externalTemp.extTempHumid(sync)
         #initialize config file with lat and lon
         conf = Config("config.json")
@@ -53,25 +105,25 @@ try:
         lat = conf.read("lat")
         lon = conf.read("lon")
         if lat != 0 and lon != 0:
-            print(f"Test 3: Success. Obtained latitude and longitude from {externalTemp.externalLatLonAPI}")
+            print(f"Success. Obtained latitude and longitude from {externalTemp.externalLatLonAPI}")
             print(f"Latitude: {lat}\nLongitude: {lon}")
             print("**************************")
         else:
-            print(f"Test 3: Failed. Could not obtain latitude and longitude from {externalTemp.externalLatLonAPI}")
+            print(f"Failed. Could not obtain latitude and longitude from {externalTemp.externalLatLonAPI}")
             print("**************************")
         
-        print("\n*******API Test 4*********")
+        print("\n*******API Test 7*********")
         conf.write("tempoutdoor",0)
         conf.write("humidoutdoor",0)
         tempHumid.updateOutdoorTemp()
         tempoutdoor = conf.read("tempoutdoor")
         humidoutdoor = conf.read("humidoutdoor")
         if tempoutdoor != 0 and humidoutdoor != 0:
-            print(f"Test 4: Success. Obtained outdoor temperature and humidity from {externalTemp.externalOpenMeteoAPI}")
+            print(f"Success. Obtained outdoor temperature and humidity from {externalTemp.externalOpenMeteoAPI}")
             print(f"Outdoor temperature: {tempoutdoor} C\nOutdoor humidity: {humidoutdoor} %")
             print("**************************")
         else:
-            print(f"Test 4: Failed. Could not obtain outdoor temperature and humidity from {externalTemp.externalOpenMeteoAPI}")
+            print(f"Failed. Could not obtain outdoor temperature and humidity from {externalTemp.externalOpenMeteoAPI}")
             print("**************************")
 except Exception as e:
     print(f"An error occurred: {e}")
