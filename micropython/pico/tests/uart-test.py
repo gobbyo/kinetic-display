1de926d0f42301eb7bea0a6e54848283c6c5ff0e
gobbyo <jbeman@hotmail.com>
2026-03-22 11:12:59 -0700

    updated tests

---
+++ micropython/pico/tests/uart-test.py
@@ -0,0 +1,22 @@
+from uart_protocol import uartProtocol, uartChannel, uartCommand, commandHelper, uartActions
+import time
+
+
+
+def main():
+
+    while True:
+        for u in range(10):
+            for i in range(2):
+                print("UART{0} test".format(i))
+                uart = uartProtocol(i, commandHelper.baudRate[3])
+                uart.clearQueue()
+                cmdStr = '0{0}{1}'.format(i,commandHelper.encodeHex(u))
+                print("sending command: {0}".format(cmdStr))
+                cmd = uartCommand(cmdStr)
+                uart.sendCommand(cmd)
+                time.sleep(1)         
+                del(uart)
+
+if __name__ == "__main__":
+    main()
